afe1b265d987207f77a35a7bf892d411b9943461
Umesh Singh Verma <119877487+umeshSinghVerma@users.noreply.github.com>
2025-04-17 10:51:05 +0530

    bug fixes

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,15 +1,21 @@
 import ChatbotUI from '@/components/Chatbot';
 
-async function getChatbotInfo(id:string){
-  const response = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/getChatbotInfo?id=${id}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },  
-  });
-  const data = await response.json();
-  return data.chatbot;
+async function getChatbotInfo(id: string) {
+  console.log("id", id);
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN_URL}/api/getChatbotInfo?id=${id}`, {
+      method: 'GET',
+    });
+
+    const data = await response.json();
+    console.log("data ",data);
+    return data.chatbot;
+  } catch (error) {
+    console.error("Error in getChatbotInfo:", error);
+    return null;
+  }
 }
+
 
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
